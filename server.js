f0a6cd23e5da94773b06ae510d85fee7e9bb6f67
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-09 08:38:18 -1100

    update.

--- server.js
+++ server.js
@@ -12,3 +12,4 @@ app.use(express.json());
 app.get('/', (req, res) => res.send("Server is running"))
 
 const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
