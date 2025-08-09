6c6c42be06bb22086fa3aaa0897cc9ca0f8d9435
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-09 08:15:11 -1100

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+// Lets Initialize Express App
+const app = express()
+
+//Middleware
+app.use(cors())
+app.use(express.json());
+
+app.get('/', (req, res) => res.send("Server is running"))
+
+const PORT = process.env.PORT
