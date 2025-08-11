d1a828b8dfa0c77f08c6926484cc5ea913bd468b
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-11 04:05:46 -1100

    update

---
+++ models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userShema = new mongoose.Schema({
+  nmae: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["owner", "user"], default: "user" },
+  image: { type: String, default: '' },
+},
+{
+    timeStamp: true
+
+});
