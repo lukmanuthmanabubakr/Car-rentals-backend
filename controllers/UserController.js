35947714cb1b1eeeeaf753a1275cb468f7ed5b70
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-12 01:20:08 +0100

    update

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,15 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+
+
+
+
+//Generate JWT Token
+const generateToken = (userId) => {
+    const payload = userId;
+    return jwt.sign(payload, process.env.JWT_SECRET)
+}
 
 export const RegisterUser = async (req, res) => {
   try {
@@ -17,6 +27,6 @@ export const RegisterUser = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({name, email, password: hashedPassword})
 
-    
+
   } catch (error) {}
 };
