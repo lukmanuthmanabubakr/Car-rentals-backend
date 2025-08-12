cf277d7ea5bb4474ea44fde36383e0c2787b6639
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-12 19:07:13 +0100

    backend all the way

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { loginUser, RegisterUser } from "../controllers/UserController.js";
+import { getUser, loginUser, RegisterUser } from "../controllers/UserController.js";
+import { protect } from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", RegisterUser);
 userRouter.post("/login", loginUser);
+userRouter.get("/data", protect, getUser);
+
 
 export default userRouter;
