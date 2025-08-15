ca95f7771c9552c36cdd005f1c3e4b89450b142a
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-15 16:08:09 +0100

    update

--- routes/OwnerRoutes.js
+++ routes/OwnerRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { addCar, changeRoleToOwner } from "../controllers/OwnerController.js";
 import { protect } from "../middleware/auth.js";
+import upload from "../middleware/Multer.js";
 
 const ownerRouter = express.Router()
 
 ownerRouter.post("/change-role", protect, changeRoleToOwner)
-ownerRouter.post("/add-car", protect, addCar)
+ownerRouter.post("/add-car", upload.single("image"), protect, addCar)
 
 
 export default ownerRouter;
