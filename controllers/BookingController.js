dfbcef45865a2a05513d5cb5a7deb4fcd48e1b30
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-17 18:42:16 +0100

    update

---
+++ controllers/BookingController.js
@@ -0,0 +1,11 @@
+import Booking from "../models/Booking";
+
+//Function to check Availability Of Car for a given date
+export const checkAvailability = async (car, pickupDate, returnDate) => {
+  const bookings = await Booking.find({
+    car,
+    pickupDate: { $lte: returnDate },
+    returnDate: { $gte: pickupDate },
+  });
+  return bookings.length === 0;
+};
