bbc1340459bf49d072c681e0849824d0a47588f0
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-18 13:48:58 +0100

    update

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -1,11 +1,51 @@
 import Booking from "../models/Booking";
+import Car from "../models/Car.js";
 
 //Function to check Availability Of Car for a given date
-export const checkAvailability = async (car, pickupDate, returnDate) => {
+const checkAvailability = async (car, pickupDate, returnDate) => {
   const bookings = await Booking.find({
     car,
     pickupDate: { $lte: returnDate },
     returnDate: { $gte: pickupDate },
   });
   return bookings.length === 0;
+};
+
+// To check Availability of cars for the given date and location
+
+export const checkAvailabilityOfCar = async (req, res) => {
+  try {
+    const { location, pickupDate, returnDate } = req.body;
+    //Fetch all cars available for the given location
+    const cars = await Car.find({ location, isAvailable: true });
+
+    //check car availability for the given date range using promises
+    const availableCarsPromises = cars.map(async (car) => {
+      const isAvailable = await checkAvailability(
+        car._id,
+        pickupDate,
+        returnDate
+      );
+      return { ...car._doc, isAvailable: isAvailable };
+    });
+
+    let availableCars = await Promise.all(availableCarsPromises);
+
+    availableCars = availableCars.filter((car) => car.isAvailable === true);
+
+    res.json({ success: true, availableCars });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//Api to Create Booking
+export const createBooking = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
 };
