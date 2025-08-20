d141779cd390f0bb0d8e804d3910b4eaf1e213a0
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-20 18:13:09 +0100

    updatw

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -95,6 +95,11 @@ export const getOwnerBookings = async (req, res) => {
     if (req.user.role !== "owner") {
       return res.json({ success: false, message: "Unauthorized" });
     }
+    const bookings = await Booking.find({ owner: req.user._id })
+      .populate("car user")
+      .select("-user.password")
+      .sort({ createdAt: -1 });
+    res.json({ success: true, bookings });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
