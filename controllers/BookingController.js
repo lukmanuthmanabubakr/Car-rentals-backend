98b0d161d24eaf86712f14c68b8217d7e237e6f0
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-25 23:53:46 +0100

    update

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -113,6 +113,9 @@ export const changeBookingStatus = async (req, res) => {
     const {bookingId, status} = req.body
 
     const booking = await Booking.findById(bookingId);
+    if(booking.owner.toString() !== _id.toString()){
+
+    }
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
