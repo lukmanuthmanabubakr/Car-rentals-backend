335196dac705d57cf570865bf41925932009dad2
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-25 23:55:56 +0100

    updated

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -110,12 +110,20 @@ export const getOwnerBookings = async (req, res) => {
 export const changeBookingStatus = async (req, res) => {
   try {
     const { _id } = req.user;
-    const {bookingId, status} = req.body
+    const { bookingId, status } = req.body;
 
     const booking = await Booking.findById(bookingId);
-    if(booking.owner.toString() !== _id.toString()){
-
+    if (booking.owner.toString() !== _id.toString()) {
+      return res.json({ success: false, message: "Unauthorized" });
     }
+
+    booking.status = status;
+    await booking.save();
+
+    res.json({
+      success: true,
+      message: "Status Updated",
+    });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
