2f7ffa72d7116e67f1e83c17de5546c55ea55bf3
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-28 00:18:54 +0100

    update

--- routes/BookingRoutes.js
+++ routes/BookingRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
+import { checkAvailabilityOfCar } from "../controllers/BookingController";
 
+const bookingRouter = express.Router();
 
-
-
-
+bookingRouter.post('check-availability', checkAvailabilityOfCar)
